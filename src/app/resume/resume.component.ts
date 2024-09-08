89849ad4fd4d0d56884cb22f9ae98191da5a9cf2
Logan Ingram <loganphx1@gmail.com>
2024-09-08 17:11:22 -0500

    - Setup Resume Page

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, Renderer2} from '@angular/core';
+import {Title} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-resume',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class ResumeComponent {
 
+  isWorkExperienceOpen: boolean = false;
+  isEducationOpen: boolean = false;
+  isCertificationsOpen: boolean = false;
+  isSkillsOpen: boolean = false;
+
+  constructor(private titleService: Title, private renderer: Renderer2) {
+    this.titleService.setTitle("Logan Ingram - Portfolio");
+  }
+
+  DownloadFile() {
+    const link = this.renderer.createElement('a');
+    link.setAttribute('target', '_blank');
+    link.setAttribute('href', 'assets/resume.pdf');
+    link.setAttribute('download', 'resume.pdf');
+    link.click();
+    link.remove();
+  }
 }
